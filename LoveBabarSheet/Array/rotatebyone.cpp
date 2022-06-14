7c7657746e2a32e2e290484496fd372760a4a175
Bhawna <bhawnabc2002@gmail.com>
2022-06-14 13:33:37 +0530

    question-2

--- LoveBabarSheet/Array/rotatebyone.cpp
+++ LoveBabarSheet/Array/rotatebyone.cpp
@@ -57,3 +57,10 @@ int main(){
 
 
 }
+/*
+        //This is also good approach
+        k = k%nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+*/
