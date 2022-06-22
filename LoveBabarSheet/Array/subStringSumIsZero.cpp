c8eeb81020f3c0c858fb20a98588e0420156b8a4
Bhawna <bhawnabc2002@gmail.com>
2022-06-22 15:02:56 +0530

    Done problem 1

--- LoveBabarSheet/Array/subStringSumIsZero.cpp
+++ LoveBabarSheet/Array/subStringSumIsZero.cpp
@@ -1,17 +1,39 @@
 // Problem - 20th
 #include <bits/stdc++.h>
 using namespace std;
+
+// Brute force approach 
+bool isSubArray(int arr[],int n){
+    for(int i=0; i < n; i++){
+        int curr_sum =0 ;
+
+        for(int j=0; j < n; j++){
+            curr_sum += arr[j];
+            if(curr_sum == 0){
+                return true;
+            }
+        }
+
+    }
+    return false;
+ 
+}
 bool subArraySumIsZero(int arr[],int n){
-    map<int,int> m;
-       m[0]++;
-       int pref=0;
-       for(int i=0; i<n; i++){
-           pref+=arr[i];
-           if(m[pref]>0)
-           return true;
-           m[pref]++;
-       }
-       return false;
+    
+    unordered_set<int>s;
+    int pre_sum = 0;
+    for(int i=0;i<n;i++){
+        pre_sum += arr[i];
+
+        if(pre_sum == 0){
+            return true;
+        }
+        if(s.find(pre_sum) != s.end()){
+            return true;
+        }
+        s.insert(pre_sum);
+    }
+    return false;
 }
 void printArray(int a[], int n)
 {
