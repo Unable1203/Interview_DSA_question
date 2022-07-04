55f827a21adfce276a7bd6ef3710c6f608f6c8c9
Bhawna <bhawnabc2002@gmail.com>
2022-07-04 13:34:09 +0530

    done

--- LoveBabarSheet/String/palindromeString.cpp
+++ LoveBabarSheet/String/palindromeString.cpp
@@ -1,3 +1,25 @@
 // Problem - 43
 #include <bits/stdc++.h>
 using namespace std;
+//BruteForceMethod = two pointer approach
+int PalindromeString1(char str[],int n){
+    int i=0;
+    int j= n-1;
+    while(i<j){
+        if(str[i] == str[j]){
+            i++;
+            j--;
+        }
+        else{
+            return 0;
+            break;
+        }
+    }
+    return 1;
+}
+int main(){
+    char arr[] = "abc";
+    int x = PalindromeString1(arr,4);
+    cout<<x<<" ";
+    return 0;
+}
