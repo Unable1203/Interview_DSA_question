4990572f5190375a8572947ffcbe27e46ab86836
bhawna <bhawnabc2002@gmail.com>
2022-09-06 07:14:22 +0530

    added the work

--- SomeBasicProblem/Sorting.cpp
+++ SomeBasicProblem/Sorting.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 /*
+// Selection Sort
 int main(){
     int array[5]={5,4,3,2,1};
     for(int i=0; i<5; i++){
@@ -18,7 +19,8 @@ int main(){
     }
     return 0;
 }
-*/
+
+//Bubble Sort
 int main(){
     int arr[6]={13,46,2,52,20,9};
 
@@ -30,6 +32,25 @@ int main(){
             }
     }
 
+    cout<<"Array after sorting: "<<endl;
+    for(int i=0;i<6;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
+*/
+//Insertion sort
+int main(){
+    int arr[6]={13,46,2,52,20,9};
+    for(int i=1;i<6;i++){
+        int current=arr[i];
+        int j=i-1;
+        while(arr[j]>current && j>=0){
+            swap(arr[j+1],arr[j]);
+            j--;
+        }
+        arr[j+1]=current;
+    }
     cout<<"Array after sorting: "<<endl;
     for(int i=0;i<6;i++){
         cout<<arr[i]<<" ";
