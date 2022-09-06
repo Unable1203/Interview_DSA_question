9ee257f773a325363482b2669a94e4bca433e147
bhawna <bhawnabc2002@gmail.com>
2022-09-06 07:01:13 +0530

    adding the work

--- SomeBasicProblem/Sorting.cpp
+++ SomeBasicProblem/Sorting.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+/*
 int main(){
     int array[5]={5,4,3,2,1};
     for(int i=0; i<5; i++){
@@ -14,6 +15,24 @@ int main(){
     cout<<"array after sorting: "<<endl;
     for(int i=0; i<5; i++){
         cout<<array[i]<<" ";
+    }
+    return 0;
+}
+*/
+int main(){
+    int arr[6]={13,46,2,52,20,9};
+
+    for(int i=0;i<6;i++){
+            for(int j=0;j<6;j++){
+                if(arr[j]> arr[j+1]){
+                    swap(arr[j],arr[j+1]);
+                }
+            }
+    }
+
+    cout<<"Array after sorting: "<<endl;
+    for(int i=0;i<6;i++){
+        cout<<arr[i]<<" ";
     }
     return 0;
 }
