be0a127d7906210924bf8cdc66db060f721a786c
bhawna <bhawnabc2002@gmail.com>
2022-09-20 12:15:02 +0530

    Skills

--- SomeBasicProblem/array/secLar.cpp
+++ SomeBasicProblem/array/secLar.cpp
@@ -1,37 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int arr[]= {0,1,2,3,4,5,6,7};
+    int arr[]= {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int n= sizeof(arr)/sizeof(arr[0]);
-    /*
-    // O(nlogn) and O(1) space
+    if(n<3){
+        cout<<"No second largest element";
+        return 0;
+    }
+   // ------Method 1: Using Sorting-------
     sort(arr,arr+n);
-    cout<<arr[n-1]<<endl;
-    */
-
-    /*
-    // good approach O(n) and O(1) space
+    cout<<"Second Smallest element : "<<arr[1]<<endl;
+    cout<<"Second Largest element : "<<arr[n-2]<<endl;
+    // ------Method 2: Using Min and Max-------
+    int secMax = arr[1];
     int max = arr[0];
-    for(int i=1; i< n; i++){
-        if(arr[i]>max){
-            max= arr[i];
+    for(int i=1; i < n; i++){
+        if(arr[i] > max){
+            secMax = max;
+            max = arr[i];
         }
-    }
-    cout<<max<<endl;
-    */
+        else if(arr[i] > secMax && arr[i] != max){
+            secMax = arr[i];
+        }
+        else{
+            secMax = -1;
+        }
 
-    // O(n) and O(1) space
-    int max= arr[0];
-    int secMax= arr[0];
-    for(int i=1; i< n; i++){
-        if(arr[i]>max){
-            secMax= max;
-            max= arr[i];
-        }
-        else if(arr[i]>secMax){
-            secMax= arr[i];
-        }
     }
-    cout<<secMax<<endl;
+    cout<<"Second Largest element : "<<secMax<<endl;
     return 0;
+   
 }
